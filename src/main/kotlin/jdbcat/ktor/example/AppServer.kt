77e97c19763b31e18f6738e79fcf90d92e1ef4a6
Santiago Battaglino <santiago.battaglino@talosdigital.com>
2021-01-09 21:24:15 -0300

    setting up concurrency and dev

--- src/main/kotlin/jdbcat/ktor/example/AppServer.kt
+++ src/main/kotlin/jdbcat/ktor/example/AppServer.kt
@@ -2,6 +2,8 @@ package jdbcat.ktor.example
 
 import com.typesafe.config.ConfigFactory
 import io.ktor.application.Application
+import io.ktor.application.ApplicationStarted
+import io.ktor.application.ApplicationStopped
 import io.ktor.application.install
 import mu.KotlinLogging
 import org.koin.Logger.SLF4JLogger
@@ -11,13 +13,20 @@ import org.koin.ktor.ext.Koin
 const val serviceApiVersionV1 = "api/v1"
 
 private val logger = KotlinLogging.logger { }
-
+// TODO implement https://dev.to/viniciusccarvalho/graceful-shutdown-of-ktor-applications-1h53 for stop ktor
 /**
  * Application's ENTRY POINT.
  * This method is called by Ktor and this entry point is configured in /resources/application.conf
  */
 @Suppress("unused")
 fun Application.main() {
+
+    environment.monitor.subscribe(ApplicationStarted){
+        println("ApplicationStarted")
+    }
+    environment.monitor.subscribe(ApplicationStopped){
+        println("ApplicationStopped")
+    }
 
     val mainConfig = ConfigFactory.load("main.conf")
     // Add Koin DI support to Ktor
