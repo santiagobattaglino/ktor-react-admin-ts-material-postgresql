4f7059df5346c4a167a9a15a47a6df8ae8fedcff
Santiago Battaglino <santiago.battaglino@talosdigital.com>
2021-02-28 04:35:06 -0300

    working with heroku

--- client/admin/src/App.tsx
+++ client/admin/src/App.tsx
@@ -25,7 +25,7 @@ import StockEdit from './components/stock/StockEdit';
 import StockList from './components/stock/StockList';
 import { Dashboard } from './dashboard';
 
-// https://sleepy-beach-97825.herokuapp.com/ + process.env.PORT || process.env.REACT_APP_PORT || 1260
+// https://sleepy-beach-97825.herokuapp.com/static + process.env.PORT || process.env.REACT_APP_PORT || 1260
 function App() {
     return (
         <Admin dashboard={Dashboard} dataProvider={simpleRestProvider('http://localhost:8080', fetchUtils.fetchJson, 'X-Total-Count')}>
